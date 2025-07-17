05e02393a3748c34ac2d54a192eab7ef88b9ceed
elfy <161840097+elfaria-473@users.noreply.github.com>
2025-07-17 20:48:49 +0800

    Update welcome_message.php
    modify text

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -23,6 +23,9 @@
     <a href="#" class="inline-block px-6 py-3 bg-green-500 text-white rounded-lg hover:bg-green-600 transition">
       Get Started
     </a>
+        <a href="#" class="inline-block px-6 py-3 bg-green-500 text-white rounded-lg hover:bg-green-600 transition">
+      Go Home!
+    </a>
   </div>
 </body>
 </html>
